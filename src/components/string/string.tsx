07b97f656756a41968190cb06d6c5fc411b49610
randomu3 <demiz99@mail.ru>
2022-12-04 00:07:56 +0500

    added string component and have question

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,57 @@
 import React from "react";
+import { wait } from "../../utilities/utilities";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import stringStyles from "./string.module.css";
+
 export const StringComponent: React.FC = () => {
+  const [isLoader, setLoader] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [vizualization, setVizualization] = React.useState<string[]>([]);
+
+  async function onClickHandler(): Promise<void> {
+    setLoader(true);
+    const array = text.split('')
+    setVizualization(array)
+    for (let i = 0; i < (array.length / 2); i++) {
+      console.log(array)
+      let tmp = array[i]
+      array[i] = array[array.length - i - 1]
+      array[array.length - i - 1] = tmp
+      await wait(1000)
+      setVizualization(array)
+    }
+    setLoader(false);
+  }
+
+  function onChangeHandler(e: React.FormEvent<HTMLInputElement>): void {
+    const value = e.currentTarget.value;
+    if (value.length <= 11) {
+      setText(value);
+    }
+  }
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={stringStyles.container}>
+        <div className={stringStyles.input}>
+          <Input value={text} onChange={onChangeHandler}></Input>
+          <Button
+            isLoader={isLoader}
+            onClick={onClickHandler}
+            text="Развернуть"
+          ></Button>
+        </div>
+        <p className={stringStyles.subtitle}>Максимум — 11 символов</p>
+      </div>
+      <div className={stringStyles.vizualization}>
+        {vizualization.map((letter) => {
+          return <Circle key={letter} letter={letter}></Circle>;
+        })}
+      </div>
     </SolutionLayout>
   );
 };
