a020991f1f3ac496d71568bb5ceaf53335938398
randomu3 <demiz99@mail.ru>
2023-02-02 17:55:58 +0500

    del app.cy.ts

--- cypress/e2e/app.cy.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-describe("Приложение корректно запускается:", function () {
-    beforeEach(function () {
-      cy.visit("http://localhost:3000/");
-    });
-  });
-  
